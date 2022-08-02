6835356451b25689a2fbca5d8d35049e6cb4498d
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-02 23:21:44 +0100

    Completed Mini Instagram App with React

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: "./",
-  globPatterns: ["**/*.{html,json,svg,png,txt,md,css,jsx,js}"],
-  swDest: "sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-};
