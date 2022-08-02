ee640a9270307e473961f1c01b9b8b43e42e8ddd
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-02 23:08:18 +0100

    Added Workbox Service Worker

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: "./",
+  globPatterns: ["**/*.{html,json,svg,png,txt,md,css,jsx,js}"],
+  swDest: "sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+};
