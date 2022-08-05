9772732af9d94ba63fc8e3f31208ede1a9d61710
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-05 20:10:40 +0100

    Added delete modal to Gallery

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import styles from "./Modal.module.css";
 import { RiCloseLine } from "react-icons/ri";
 
-function Modal({ setIsOpen, deleteBtn }) {
+function Modal(props) {
   return (
     <>
-      <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+      <div className={styles.darkBG} onClick={() => props.setIsOpen(false)} />
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
             <h5 className={styles.heading}>Delete Photo</h5>
           </div>
-          <div className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+          <div
+            className={styles.closeBtn}
+            onClick={() => props.setIsOpen(false)}
+          >
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </div>
           <div className={styles.modalContent}>
             Are you sure you want to delete this Photo?
           </div>
           <div className={styles.modalActions}>
-            <button className={styles.deleteBtn} onClick={deleteBtn}>
+            <button className={styles.deleteBtn} onClick={props.deleteBtn}>
               Delete
             </button>
             <button
               className={styles.cancelBtn}
-              onClick={() => setIsOpen(false)}
+              onClick={() => props.setIsOpen(false)}
             >
               Cancel
             </button>
