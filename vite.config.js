3e5c8634b8c4e011d12ac7d02c8e18ed15ec7f91
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-11 00:47:10 +0100

    Made changes to service worker

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        globPatterns: ["**/*.{js,jsx,css,html,ico,png,svg}"],
         runtimeCaching: [
           getCache({
             pattern: /^https:\/\/kit.fontawesome.com\/23a2825686.js/,
