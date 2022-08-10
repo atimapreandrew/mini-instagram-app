c89290543ee2b7070274c753cc24eb63eed6687d
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-10 23:05:23 +0100

    Added service worker

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const getCache = ({ name, pattern }) => ({
+  urlPattern: pattern,
+  handler: "CacheFirst",
+  options: {
+    cacheName: name,
+    expiration: {
+      maxEntries: 500,
+      maxAgeSeconds: 60 * 60 * 24 * 365 * 2, // 2 years
+    },
+    cacheableResponse: {
+      statuses: [200],
+    },
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          getCache({
+            pattern: /^https:\/\/kit.fontawesome.com\/23a2825686.js/,
+            name: "font-awesome",
+          }),
+        ],
+      },
     }),
   ],
 });
