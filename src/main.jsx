8ff0ee9cfb4b1108695c8ef3e16a44a8e481936e
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-10 21:38:29 +0100

    Added service worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -7,3 +8,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
